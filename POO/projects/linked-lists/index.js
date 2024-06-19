d9d2c99fbfccf6297e96c432a8a88de8354931ab
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-19 15:27:08 +0200

    :construction: wip: create add and pop method by creating a new reference to the LinkedList head prop and recursive checking in it - POO final project about linked list algorythm

--- POO/projects/linked-lists/index.js
+++ POO/projects/linked-lists/index.js
@@ -1,43 +1,59 @@
-class Nodee {
+
+class ListNode {
   constructor(value) {
     this.value = value;
-    this.nextNode = null;
+    this.next = null;
   }
 }
 
-const firstNode = new Nodee(7);
-firstNode.nextNode = new Nodee(8);
-console.log(firstNode);
+// let headNode = new ListNode(10);
+// console.log({value: headNode.value, nextNode: headNode.next});
+// headNode.next = new ListNode(20);
+// console.log({value: headNode.value, nextNode: headNode.next});
+// headNode.next.next = new ListNode(30);
+// console.log({value: headNode.value, nextNode: headNode.next});
+
+// while(headNode.next.next){
+//   headNode = headNode.next
+//   console.log(headNode.next)
+// }
+
+// headNode.next = null
+
+// console.log(headNode)
 
 class LinkedList { 
-  constructor() {
-    this.head = null;
+  constructor(node) {
+    this.head = node;
   }
 
-  add(value) {
-    return new Nodee(value)
-  }
-  pop(){
-
-  }
-
-  delete(index){
-
-  }
-
-  insertAt(index, node){
-
+  add(node) {
+    let headCopy = this.head
+    //while the linked list has a next node
+    //go to the next node until it's null
+    while(headCopy.next){ 
+      headCopy = headCopy.next
+    }
+    //if the linked list doesn't have a next node
+    //add the node to the end of the list
+    headCopy.next = new ListNode(node)
+    return this
   }
 
-  find(value){
-    return
-  }
-  
-  get count(){
-    return
-  }
-
-  get displayList(){
-    return
+  pop() {
+    let headCopy = this.head
+    //while the linked list has a next node with a next node
+    //go to the next node with a null next
+    while(headCopy.next.next){ 
+      headCopy = headCopy.next
+    }
+    //if the linked list have a next node with a null next
+    //set the next node to null
+    headCopy.next = null
+    return this
   }
 }
+
+const linkedList = new LinkedList(new ListNode(10))
+linkedList.add(20).add(30).pop().add(5)
+console.log({LIST_FINALE:linkedList.head, head: linkedList.head.next});
